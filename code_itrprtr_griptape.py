9fbd401bc00a28d4a0e05d6fbfbefe517933656f
Occupying-Mars <72188133+Occupying-Mars@users.noreply.github.com>
2024-06-01 00:45:41 -0700

    sdk refactor changes

--- code_itrprtr_griptape.py
+++ code_itrprtr_griptape.py
@@ -1,13 +1,13 @@
 from griptape.structures import Agent
 from griptape.utils import Chat
 
-from composio_griptape import App, Action, Tag, ComposioToolset
+from composio_griptape import App, Action, Tag, ComposioToolSet
 import dotenv
 
 
 dotenv.load_dotenv()
 
-composio_toolset = ComposioToolset()
+composio_toolset = ComposioToolSet()
 composio_tools = composio_toolset.get_actions(actions=Action.CODEINTERPRETER_EXECUTE_CODE)
 
 agent = Agent(
